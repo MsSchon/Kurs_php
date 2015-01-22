3a9dbc782c0a3ec5b2b45677beb6a778c79081fe
MsSchon <magdalena.schon@gmail.com>
2015-01-22 17:19:50 +0100

    nu syns knappen igen.
    efter att kopierat kod från gårdagens lektion så försvann den, så nu har
    jag kommenterat bort all ny kod. Alla namnen är ju annorlunda så vet
    inte vad som är vad.

--- assign7/button.php
+++ assign7/button.php
@@ -32,21 +32,23 @@ class MS_Button
     return $clicks / $views;
   }
 
+
   /*
   *Skriv datan tll filen och dela upp på radbryt
   */
+  /*
   public function loadData()
   {
     $content = file_get_contents($this->storagePath);
     $data = explode("\n", $content);
     return $data;
-  }
+  }*/
 
 
   /*
   * för att räkna alla klick på knappen
   */
-  public function trackClick() {
+  public function trackClick() { /*
     $data = $this->loadData();
     for ($index = 1; $index < count($data); $index++){
       $optionRow = $data[$index];
@@ -57,46 +59,80 @@ class MS_Button
         $data[$index] = $optionRow;
         break;
       } 
+      }*/
     }
-    $this->saveData($data);
-  }
+   // $this->saveData($data);
+  
 
   /*
   * för att räkna alla visningar
   */
-    public function trackView(/*$option*/) { 
-      $data = $this->loadData();
-      for ($index = 1; $index < count($data); $index++){
-        $optionRow = $data[$index];
-        $optionData = expolode(',' , $optionRow);
-        if ($option == $optionData[0]) {
-          $optionData[2] = $optiondata[2] +1;
-          $optionRow = implode(',' , $optionData);
-          $data[$index] = $optionRow;
-          break;
+  /*public function trackView($option) { 
+    $data = $this->loadData();
+    for ($index = 1; $index < count($data); $index++){
+      $optionRow = $data[$index];
+      $optionData = expolode(',' , $optionRow);
+      if ($option == $optionData[0]) {
+        $optionData[2] = $optiondata[2] +1;
+        $optionRow = implode(',' , $optionData);
+        $data[$index] = $optionRow;
+        break;
       }
-      $this->saveData($data);
-    }
-    
-    }
-  }
+      //$this->saveData($data);
+    } 
 
-  $myButton = new MS_Button();
-  $views = 1;
-  $clicks = 1;
-  $options = 0;
+  }*/
+}
 
-  $row = $clicks . " clicks," . $views . " views," . $options . "\n";
+$myButton = new MS_Button();
+$views = 1;
+$clicks = 1;
+$options = 0;
 
+$row = $clicks . " clicks," . $views . " views," . $options . "\n";
 
-  /*
+/*
+*skriver data till fil, 
+* ska skriva till fil men saknar en massa saker som jag inte
+*förstår. Försökte ändra den så att det skulle passa min fil
+* men det gick inte så bra. 
+*/
+/*  public function saveData($data) 
+    $row = implode($data, "\n");
+    $handle =fopen($this->storagePath, 'w+');
+
+    if ($handle === false) {
+      return false;
+      }
+      $result = fwrite($handle, $row);
+      fclose($handle)
+    */
+/*
   *Spara resultatet till en CSV-fil. 
   */
 
-  //Öppna och skriv till fil
-  $my_file = 'data.csv';
-  $handle = fopen($my_file,'w+') or die('Cannot open file:  '.$my_file);
-  fwrite($handle, $row); 
-  fclose($handle);
+//Öppna och skriv till fil
+$my_file = 'data.csv';
+$handle = fopen($my_file,'w+') or die('Cannot open file:  '.$my_file);
+fwrite($handle, $row); 
+fclose($handle);
+
+/*$msButton = new MS_Button();
+$msButton -> initializeStorage();
+
+$button = new MS_Button();
+$button->initilalizeStorage():
+$option = $button->getOption();
+$button->trackView($option);
+*/
+/*
+*om man klickat på knappen så ska datan med valen skickas,
+*annars ska bara view öka. dvs man har uppdaterat sidan
+*manuellt.
+*/
+/*
+if(array_key_exists('option', $_POST)){
+     $button->trackClick($POST['option']);
+}*/
 
 ?>
